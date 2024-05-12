b948d7ebd088292e082bd76f8aa58919f05a40db
MrHuangJser <mrhuangjser@gmail.com>
2024-05-12 21:31:06 +0800

    code format

--- lua/plug.lua
+++ lua/plug.lua
@@ -1,26 +1,25 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-
   use("wbthomason/packer.nvim")
-  
-  
+
+
   use({
     "kylechui/nvim-surround",
     tag = "*", -- Use for stability; omit to use `main` branch for the latest features
     config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
     end
   })
-  
+
   use("tpope/vim-commentary")
-  
+
   use("justinmk/vim-sneak")
-  
+
   use("chrisgrieser/nvim-spider")
-  
+
   use {
     'phaazon/hop.nvim',
     tag = "*",
@@ -28,5 +27,5 @@ return require('packer').startup(function(use)
       require('hop').setup()
     end
   }
-  
+
 end)
